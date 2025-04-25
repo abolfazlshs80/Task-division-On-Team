3f6f4774f03a7aaf76465cfc81e551ab1a26b784
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-04-25 12:56:06 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_division_On_Team
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
